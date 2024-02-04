037c99c2ae01d806ea167e0bc0ba157c059235f2
FTMFR <fatemefarajzade018@gmail.com>
2024-02-04 11:21:40 +0330

    add delete api

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,17 +1,42 @@
 import React, { useEffect, useState } from "react";
-import { Table } from "react-bootstrap";
+import { Button, Modal, Table } from "react-bootstrap";
+import { MdDelete } from "react-icons/md";
+import { MdEditDocument } from "react-icons/md";
 
 const Users = () => {
   const [users, setUsers] = useState([]);
+  const [modal, setModal] = useState(false);
+  const [specificUser, setSpecificUser] = useState("");
+  const [getData, setGetData] = useState(false);
 
-  useEffect(() => {
-    fetch("https://fir-demo-8d3af-default-rtdb.firebaseio.com/usersInfo.json")
+  useEffect(async () => {
+   await fetch("https://fir-demo-8d3af-default-rtdb.firebaseio.com/usersInfo.json")
       .then((res) => res.json())
       .then((data) => {
         console.log(data);
         setUsers(Object.entries(data));
       });
-  }, []);
+  }, [getData]);
+
+  const deleteHandler = (e) => {
+    setModal(true);
+    setSpecificUser(e.target.value);
+  };
+
+  const removeHandler = async (e) => {
+    console.log(e.target);
+
+    await fetch(
+      `https://fir-demo-8d3af-default-rtdb.firebaseio.com/usersInfo/${specificUser}.json`,
+      {
+        method: "DELETE",
+      }
+    ).then((res) => res.json());
+
+    setModal(false);
+
+    setGetData((prevState) => !prevState);
+  };
 
   return (
     <div className="table-container">
@@ -22,6 +47,7 @@ const Users = () => {
             <th>First Name</th>
             <th>Last Name</th>
             <th>Email</th>
+            <th>Actions</th>
           </tr>
         </thead>
         <tbody>
@@ -31,10 +57,36 @@ const Users = () => {
               <td>{user[1].firstName}</td>
               <td>{user[1].lastName}</td>
               <td>{user[1].email}</td>
+              <td style={{ cursor: "pointer" }}>
+                <MdDelete value={user[0]} onClick={(e) => deleteHandler(e)} />
+                <MdEditDocument />
+              </td>
             </tr>
           ))}
         </tbody>
       </Table>
+
+      <Modal className="modal" show={modal}>
+        <Modal.Header closeButton>
+          <Modal.Title>Delete Confirm</Modal.Title>
+        </Modal.Header>
+
+        <Modal.Body>
+          <p>Are You Sure to Delete?</p>
+        </Modal.Body>
+
+        <Modal.Footer>
+          <Button variant="secondary" onClick={() => setModal(false)}>
+            No
+          </Button>
+          <Button
+            variant="primary"
+            onClick={(e) => removeHandler(e.target.value)}
+          >
+            Yes
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </div>
   );
 };
